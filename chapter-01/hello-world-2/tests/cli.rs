64e410f121abf9409e756e00b5eeae16d476b102
Eliseu Videira <eliseuvideira1@gmail.com>
2022-09-10 14:32:02 -0300

    :sparkles: Writing true and false binaries

--- chapter-01/hello-world-2/tests/cli.rs
+++ chapter-01/hello-world-2/tests/cli.rs
@@ -6,5 +6,19 @@ use assert_cmd::prelude::{CommandCargoExt, OutputAssertExt};
 fn runs() {
     let mut cmd = Command::cargo_bin("main").unwrap();
 
+    cmd.assert().success().stdout("Hello, world!\n");
+}
+
+#[test]
+fn true_ok() {
+    let mut cmd = Command::cargo_bin("true").unwrap();
+
     cmd.assert().success();
+}
+
+#[test]
+fn false_not_ok() {
+    let mut cmd = Command::cargo_bin("false").unwrap();
+
+    cmd.assert().failure();
 }
